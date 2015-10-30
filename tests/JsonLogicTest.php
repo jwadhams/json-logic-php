4689f607657851d348a77d564331beba424d7eda
Jeremy Wadhams <jwadhams@dealerinspire.com>
2015-10-30 13:31:33 -0500

    Move to shared tests we curl down from JsonLogic.com and clean up comments

--- tests/JsonLogicTest.php
+++ tests/JsonLogicTest.php
@@ -24,16 +24,36 @@ class JsonLogicTest extends PHPUnit_Framework_TestCase{
     {
 		ini_set('auto_detect_line_endings',TRUE);
 		
+		$common_handle = fopen("php://temp", "rw");
+		
+		$c = curl_init();
+		curl_setopt($c, CURLOPT_URL, 'http://jsonlogic.com/tests.csv');
+		curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
+		
+		fwrite($common_handle, curl_exec($c));
+
+		$code = curl_getinfo($c, CURLINFO_HTTP_CODE);
+		
+		if($code !== 200){
+			die("Could not download shared tests from jsonlogic.com: {$code} \n");
+		}
+		curl_close($c);
+		rewind($common_handle);
+		
+		/*
 		$common_handle = fopen(__DIR__ . "/tests.csv", "r");
 		if(!$common_handle) die("Couldn't open common tests");
+		*/
 		
 		$common_tests = [];
 		while( false !== ( $row = fgetcsv($common_handle)  )){
 			if($row and count($row) >= 3){
-				//Every scenario is double testsed, once using PHP associative arrays:
-				$common_tests[] = array_map('json_decode', $row, array_fill(0, count($row), true));
-				//Every scenario is double testsed, once using PHP objects:
+				//Every scenario is double tested
+				//once using PHP objects:
 				$common_tests[] = array_map('json_decode', $row);
+				//once using PHP associative arrays:
+				$common_tests[] = array_map('json_decode', $row, array_fill(0, count($row), true));
 			}
 		}
 		fclose($common_handle);
