cc098e86f12a436699dd75e8e35f6397d56829f6
Jeremy Wadhams <jwadhams@dealerinspire.com>
2015-11-03 22:43:18 -0600

    Switch up to the JSON formatted shared tests

--- tests/JsonLogicTest.php
+++ tests/JsonLogicTest.php
@@ -11,7 +11,7 @@ class JsonLogicTest extends PHPUnit_Framework_TestCase{
     }
 
 	/**
-     * @dataProvider csvProvider
+     * @dataProvider commonProvider
      */
 	public function testCommon($logic, $data, $expected)
     {
@@ -20,45 +20,34 @@ class JsonLogicTest extends PHPUnit_Framework_TestCase{
     }
 
 
-    public function csvProvider()
+    public function commonProvider()
     {
-		ini_set('auto_detect_line_endings',TRUE);
-		
-		$common_handle = fopen("php://temp", "rw");
-		
 		$c = curl_init();
-		curl_setopt($c, CURLOPT_URL, 'http://jsonlogic.com/tests.csv');
+		curl_setopt($c, CURLOPT_URL, 'http://jsonlogic.com/tests.json');
 		curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
 		
-		fwrite($common_handle, curl_exec($c));
+		$body = curl_exec($c);
 
 		$code = curl_getinfo($c, CURLINFO_HTTP_CODE);
 		
 		if($code !== 200){
 			die("Could not download shared tests from jsonlogic.com: {$code} \n");
 		}
+
 		curl_close($c);
-		rewind($common_handle);
 		
-		/*
-		$common_handle = fopen(__DIR__ . "/tests.csv", "r");
-		if(!$common_handle) die("Couldn't open common tests");
-		*/
+		//Every scenario is double tested
+		$common_tests = array_merge(
+			json_decode($body),//once using PHP objects
+			json_decode($body, true)//once using PHP associative arrays
+		);
+		$common_tests = array_filter($common_tests, function($row){
+			//Discard comments or malformed rows
+			return is_array($row) and count($row) >= 3;
+		});
 		
-		$common_tests = [];
-		while( false !== ( $row = fgetcsv($common_handle)  )){
-			if($row and count($row) >= 3){
-				//Every scenario is double tested
-				//once using PHP objects:
-				$common_tests[] = array_map('json_decode', $row);
-				//once using PHP associative arrays:
-				$common_tests[] = array_map('json_decode', $row, array_fill(0, count($row), true));
-			}
-		}
-		fclose($common_handle);
 		return $common_tests;
 	}
-
 
 }
