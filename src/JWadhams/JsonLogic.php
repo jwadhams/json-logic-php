cf3c30fdf91fd27d5a9332b06daccabef1de9d53
Jeremy Wadhams <jwadhams@dealerinspire.com>
2016-02-26 15:11:26 -0600

    Adding Missing, Merge, and smarter If to replace ternary

--- src/JWadhams/JsonLogic.php
+++ src/JWadhams/JsonLogic.php
@@ -15,13 +15,17 @@ class JsonLogic
 		);
 	}
 
-    public static function apply($logic = [], $data = [])
-    {
+	public static function truthy($logic){
+		if($logic === "0") return true;
+		return (bool)$logic;
+	}
+
+	public static function apply($logic = [], $data = []) {
 		//I'd rather work with array syntax
 		if(is_object($logic)) $logic = (array)$logic;
 		if(is_object($data)) $data = (array)$data;
 
-		if(! self::is_logic($logic) ) return $logic;
+		if(! self::is_logic($logic) ){ return $logic; }
 
 		$operators = [
 			'==' => function($a, $b){ return $a == $b; },
@@ -50,7 +54,6 @@ class JsonLogic
 				foreach(func_get_args() as $a){ if($a) return $a; }
 				return $a;
 			},
-			'?:' => function($a, $b, $c){ return $a ? $b : $c; },
 			'log' => function($a){ error_log($a); return $a; },
 			'var' => function($a, $default = null) use ($data){ 
 				//Descending into data using dot-notation
@@ -68,6 +71,27 @@ class JsonLogic
 				}
 				return $data;
 			},
+			'missing' => function() use ($data){
+				/*
+					Missing can receive many keys as many arguments, like {"missing:[1,2]}
+					Missing can also receive *one* argument that is an array of keys,
+					which typically happens if it's actually acting on the output of another command 
+					(like IF or MERGE)
+				*/
+				$values = func_get_args();
+				if(!static::is_logic($values) and isset($values[0]) and is_array($values[0] ) ){
+					$values = $values[0];
+				}
+
+				$missing = [];
+				foreach($values as $data_key){
+					if(static::apply(['var'=>$data_key], $data) === null){
+						array_push($missing, $data_key);
+					}
+				}
+				
+				return $missing;
+			},
 			'in' => function($a, $b){
 				if(is_array($b)) return in_array($a, $b);
 				if(is_string($b)) return strpos($b, $a) !== false;
@@ -83,6 +107,11 @@ class JsonLogic
 			'/' => function($a,$b){ return $a / $b; },
 			'*' => function(){
 				return array_reduce(func_get_args(), function($a, $b){ return $a*$b; }, 1); 
+			},
+			'merge' => function(){
+				return array_reduce(func_get_args(), function($a, $b){ 
+					return array_merge((array)$a, (array)$b); 
+				}, []); 
 			}
 		];
 
@@ -91,8 +120,36 @@ class JsonLogic
 		$values = $logic[$op];
 
 		//easy syntax for unary operators, like ["var" => "x"] instead of strict ["var" => ["x"]]
-		$values = (array)$values; 
-	
+		if(!is_array($values) or static::is_logic($values)){
+			$values = [ $values ]; 
+		}
+
+		// 'if' violates the normal rule of depth-first calculating all the values,
+		//let it manage its own recusrion
+		if($op === 'if' || $op == '?:'){
+			/* 'if' should be called with a odd number of parameters, 3 or greater
+				This works on the pattern:
+				if( 0 ){ 1 }else{ 2 };
+				if( 0 ){ 1 }else if( 2 ){ 3 }else{ 4 };
+				if( 0 ){ 1 }else if( 2 ){ 3 }else if( 4 ){ 5 }else{ 6 };
+
+				The implementation is:
+				For pairs of values (0,1 then 2,3 then 4,5 etc)
+					If the first evaluates truthy, evaluate and return the second
+					If the first evaluates falsy, jump to the next pair (e.g, 0,1 to 2,3)
+				given one parameter, evaluate and return it. (it's an Else and all the If/ElseIf were false)
+				given 0 parameters, return NULL (not great practice, but there was no Else)
+			*/
+			for($i = 0 ; $i < count($values) - 1 ; $i += 2){
+				if( static::truthy( static::apply($values[$i], $data) ) ){
+					return static::apply($values[$i+1], $data);
+				}
+			}
+			if(count($values) === $i+1) return static::apply($values[$i], $data);
+			return null;
+		}
+
+
 		//Recursion!
 		$values = array_map(function($value) use ($data){
 			return self::apply($value, $data);
@@ -103,7 +160,7 @@ class JsonLogic
 		}
 
 		return call_user_func_array($operators[$op], $values);
-    }
+	}
 
 	public static function uses_data($logic){
 		if(is_object($logic)) $logic = (array)$logic;
