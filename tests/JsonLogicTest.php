17bccd9bacddf98a00e9ab5ad6f5815357a4aefd
Jeremy Wadhams <jwadhams@dealerinspire.com>
2016-02-29 15:16:21 -0600

    More clearly copy the tests locally. Speeds up dev, and makes it easier to write local tests while prototyping

--- tests/JsonLogicTest.php
+++ tests/JsonLogicTest.php
@@ -3,54 +3,51 @@
 class JsonLogicTest extends PHPUnit_Framework_TestCase{
 
 	/**
-     * @expectedException Exception
-     */
-    public function testInvalidOperator()
-    {
+	 * @expectedException Exception
+	 */
+	public function testInvalidOperator()
+	{
 		JWadhams\JsonLogic::apply(['fubar'=> [1,2]]);
-    }
+	}
 
 	/**
-     * @dataProvider commonProvider
-     */
+	 * @dataProvider commonProvider
+	 */
 	public function testCommon($logic, $data, $expected)
-    {
-        // Assert
-        $this->assertEquals(
+	{
+		// Assert
+		$this->assertEquals(
 			$expected, 
 			JWadhams\JsonLogic::apply($logic, $data),
 			"JsonLogic::apply(".json_encode($logic).", ".json_encode($data).") == ".json_encode($expected)
 		);
-    }
+	}
 
 
-    public function commonProvider()
-    {
-		$c = curl_init();
-		curl_setopt($c, CURLOPT_URL, 'http://jsonlogic.com/tests.json');
-		curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
-		
-		$body = curl_exec($c);
+	public function commonProvider()
+	{
+		$local_path = __DIR__ . '/tests.json';
 
-		$code = curl_getinfo($c, CURLINFO_HTTP_CODE);
-		
-		if($code !== 200){
-			die("Could not download shared tests from jsonlogic.com: {$code} \n");
+		if( ! file_exists($local_path)){
+			echo "Downloading shared tests from JsonLogic.com ...\n";
+			file_put_contents($local_path, fopen("http://jsonlogic.com/tests.json", 'r'));
+		}else{
+			echo "Using cached tests from " . @ date('r', filemtime($local_path)) ."\n";
+			echo "(rm {$local_path} to refresh)\n";
 		}
-
-		curl_close($c);
 		
+		$body = file_get_contents($local_path);
+
 		//Every scenario is double tested
 		$common_tests = array_merge(
-			json_decode($body),//once using PHP objects
-			json_decode($body, true)//once using PHP associative arrays
-		);
+				json_decode($body),//once using PHP objects
+				json_decode($body, true)//once using PHP associative arrays
+				);
 		$common_tests = array_filter($common_tests, function($row){
 			//Discard comments or malformed rows
 			return is_array($row) and count($row) >= 3;
 		});
-		
+
 		return $common_tests;
 	}
 
